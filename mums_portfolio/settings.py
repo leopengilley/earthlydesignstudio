aaac02b486497b0da479afc9f470b693d10a35e8
LioPeng <lionel.pengilley@hotmail.com>
2023-10-11 14:33:34 +1100

    Update ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS with site URL

--- mums_portfolio/settings.py
+++ mums_portfolio/settings.py
@@ -31,7 +31,9 @@ DEBUG = False
 # DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['web-production-262f3.up.railway.app/', '127.0.0.1']
+
+CSRF_TRUSTED_ORIGINS = ['https://web-production-262f3.up.railway.app/']
 
 
 # Application definition
