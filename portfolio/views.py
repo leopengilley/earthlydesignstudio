3bcf850f11ed1e6388e13506e044bf7acee678f6
LioPeng <lionel.pengilley@hotmail.com>
2023-10-13 11:51:34 +1100

    pagination 12

--- portfolio/views.py
+++ portfolio/views.py
@@ -12,7 +12,7 @@ def home(request):
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
-    return render(request, 'index.html', {'page_obj': page_obj})
+    return render(request, 'index.html', {'page_obj': page_obj, 'paginator': paginator})
 
 def about_me(request):
     return render(request, 'about_me.html')
